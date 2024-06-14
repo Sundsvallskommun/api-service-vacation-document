e825759484469599996905b47848698fd0fe4fb2
Jonas Klingstedt <jonas.klingstedt@gmail.com>
2024-06-14 15:19:08 +0200

    UF-8754 - Templating integration
    Adds a Templating integration

---
+++ src/main/java/se/sundsvall/vacationdocument/integration/templating/TemplatingClientConfiguration.java
@@ -0,0 +1,37 @@
+package se.sundsvall.vacationdocument.integration.templating;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+
+import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
+import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
+import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
+
+@Import(FeignConfiguration.class)
+@EnableConfigurationProperties(TemplatingClientProperties.class)
+class TemplatingClientConfiguration {
+
+    static final String CLIENT_ID = "templating";
+
+    @Bean
+    FeignBuilderCustomizer feignBuilderCustomizer(final TemplatingClientProperties properties) {
+        return FeignMultiCustomizer.create()
+            .withErrorDecoder(new ProblemErrorDecoder(CLIENT_ID, List.of(NOT_FOUND.value())))
+            .withRetryableOAuth2InterceptorForClientRegistration(ClientRegistration.withRegistrationId(CLIENT_ID)
+                .tokenUri(properties.oauth2().tokenUrl())
+                .clientId(properties.oauth2().clientId())
+                .clientSecret(properties.oauth2().clientSecret())
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build())
+            .withRequestTimeoutsInSeconds(properties.connectTimeoutInSeconds(), properties.readTimeoutInSeconds())
+            .composeCustomizersToOne();
+    }
+}
